c126b16eb69810bc112e5f72ce1c6ebbd62ed278
shinjuno123 <shinjuno123@naver.com>
2023-01-01 20:48:10 +0900

    Create Hands-on Examples for Spring Framework

---
+++ spring-demo-one/src/com/luv2code/springdemo/SoccerCoach.java
@@ -0,0 +1,13 @@
+package com.luv2code.springdemo;
+
+public class SoccerCoach implements Coach{
+	public String getDailyWorkout() {
+		return "Kick a ball to goal post 30 times";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
